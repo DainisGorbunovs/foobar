a235070a759e1a50569fa3d90f6a17896ae12970
Dainis <dainis.gorbunovs@student.manchester.ac.uk>
2016-09-19 02:49:54 +0100

    Case 4 added

--- test/dont_mind_the_map/AnswerTest.java
+++ test/dont_mind_the_map/AnswerTest.java
@@ -22,4 +22,116 @@ public class AnswerTest {
                 {2, 2},
         });
     }
+
+    @Test
+    public void case4test() {
+        Assert.assertEquals(-1, new int[][] {
+                {1, 2},
+                {2, 2},
+                {3, 3},
+                {4, 4},
+                {5, 5},
+                {6, 6},
+                {7, 7},
+                {8, 8},
+                {9, 9},
+                {10, 10},
+                {11, 11},
+                {12, 12},
+                {13, 13},
+                {14, 14},
+                {15, 15},
+                {16, 16},
+                {17, 17},
+                {18, 18},
+                {19, 19},
+                {20, 20},
+                {21, 21},
+                {22, 22},
+                {23, 23},
+                {24, 24},
+                {25, 25},
+                {0, 0}
+        });
+    }
+
+    /*
+    subway[station][direction]
+    4: answer is -1, there are two directions from each station
+         0: 1 and 2
+         1: 2 and 2
+         2: 3 and 3
+         3: 4 and 4
+         4: 5 and 5
+         5: 6 and 6
+         6: 7 and 7
+         7: 8 and 8
+         8: 9 and 9
+         9: 10 and 10
+        10: 11 and 11
+        11: 12 and 12
+        12: 13 and 13
+        13: 14 and 14
+        14: 15 and 15
+        15: 16 and 16
+        16: 17 and 17
+        17: 18 and 18
+        18: 19 and 19
+        19: 20 and 20
+        20: 21 and 21
+        21: 22 and 22
+        22: 23 and 23
+        23: 24 and 24
+        24: 25 and 25
+        25: 0 and 0
+    5: answer is 0, there are three directions from each station
+         0: 2, 3 and 0
+         1: 2, 4 and 1
+         2: 0, 5 and 2
+         3: 4, 6 and 3
+         4: 5, 7 and 4
+         5: 3, 8 and 5
+         6: 7, 9 and 6
+         7: 8, 10 and 7
+         8:
+         9:
+        10:
+        11:
+        12:
+        13:
+        14:
+        15:
+        16:
+        17:
+        18:
+        19:
+        20:
+        21:
+        22:
+        23:
+        24:
+        25:
+        26:
+        27:
+        28:
+        29:
+        30:
+        31:
+        32:
+        33:
+        34:
+        35:
+        36:
+        37:
+        38:
+        39:
+        40:
+        41:
+        42:
+        43:
+        44:
+        45:
+        46:
+        47: three elements
+     */
 }
