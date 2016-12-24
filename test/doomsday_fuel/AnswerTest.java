80cb4150de5959205e62706643217ad9f6cbc746
Dainis <dainis.gorbunovs@student.manchester.ac.uk>
2016-12-24 14:16:15 +0000

    Check matrix inverse from 2x2 to 7x7

--- test/doomsday_fuel/AnswerTest.java
+++ test/doomsday_fuel/AnswerTest.java
@@ -51,7 +51,7 @@ public class AnswerTest {
     }
 
     @Test
-    public void matrixTests() {
+    public void matrix2x2InversionTest() {
         double[][] input = new double[][]{
                 {1D, -1/2D},
                 {-4/9D, 1D}
@@ -60,6 +60,131 @@ public class AnswerTest {
         double[][] expectedInverse = new double[][]{
                 {9/7D, 9/14D},
                 {4/7D, 9/7D}
+        };
+
+        double[][] inverse = Answer.invert(input);
+
+        for (int index = 0; index < expectedInverse.length; ++index) {
+            Assert.assertArrayEquals(expectedInverse[index], inverse[index], 0.0000001);
+        }
+    }
+
+    @Test
+    public void matrix3x3InversionTest() {
+        double[][] input = new double[][]{
+                {1D, 2D, 3D},
+                {0D, 1D, 5D},
+                {5D, 6D, 0D}
+        };
+
+        double[][] expectedInverse = new double[][]{
+                {-6D, 18/5D, 7/5D},
+                {5D, -3D, -1D},
+                {-1D, 4/5D, 1/5D}
+        };
+
+        double[][] inverse = Answer.invert(input);
+
+        for (int index = 0; index < expectedInverse.length; ++index) {
+            Assert.assertArrayEquals(expectedInverse[index], inverse[index], 0.0000001);
+        }
+    }
+
+    @Test
+    public void matrix4x4InversionTest() {
+        double[][] input = new double[][]{
+                {1D, 1D, 1D, 0D},
+                {0D, 3D, 1D, 2D},
+                {2D, 3D, 1D, 0D},
+                {1D, 0D, 2D, 1D}
+        };
+
+        double[][] expectedInverse = new double[][]{
+                {-3D, -1/2D, 3/2D, 1D},
+                {1D, 1/4D, -1/4D, -1/2D},
+                {3D, 1/4D, -5/4D, -1/2D},
+                {-3D, 0D, 1D, 1D}
+        };
+
+        double[][] inverse = Answer.invert(input);
+
+        for (int index = 0; index < expectedInverse.length; ++index) {
+            Assert.assertArrayEquals(expectedInverse[index], inverse[index], 0.0000001);
+        }
+    }
+
+    @Test
+    public void matrix5x5InversionTest() {
+        double[][] input = new double[][]{
+                {2D, -2D, 0D, 0D, -3D},
+                {3D, -12D, 3D, 2D, -1D},
+                {0D, 9D, -2D, 0D, 2D},
+                {-1D, 2D, 0D, 3D, 0D},
+                {0D, 0D, 1D, 0D, 0D}
+        };
+
+        double[][] expectedInverse = new double[][]{
+                {53/359D, 69/359D, 114/359D, -46/359D, 21/359D},
+                {22/359D, -12/359D, 27/359D, 8/359D, 90/359D},
+                {0, 0, 0, 0, 1},
+                {3/359D, 31/359D, 20/359D, 99/359D, -53/359D},
+                {-99/359D, 54/359D, 58/359D, -36/359D, -46/359D}
+        };
+
+        double[][] inverse = Answer.invert(input);
+
+        for (int index = 0; index < expectedInverse.length; ++index) {
+            Assert.assertArrayEquals(expectedInverse[index], inverse[index], 0.0000001);
+        }
+    }
+
+    @Test
+    public void matrix6x6InversionTest() {
+        double[][] input = new double[][]{
+                {1D, 0D, 2D, 0D, 0D, 4D},
+                {18D, 1D, 5D, 0D, 0D, 9D},
+                {3D, 5D, 3D, 6D, 0D, 4D},
+                {2D, 0D, 8D, 0D, 0D, 7D},
+                {7D, 0D, 4D, 0D, 6D, 0D},
+                {0D, 0D, 1D, 0D, 0D, 0D}
+        };
+
+        double[][] expectedInverse = new double[][]{
+                {-7D, 0D, 0D, 4D, 0D, -18D},
+                {108D, 1D, 0D, -63D, 0D, 283D},
+                {0D, 0D, 0D, 0D, 0D, 1D},
+                {-527/6D, -5/6D, 1/6D, 307/6D, 0D, -230D},
+                {49/6D, 0D, 0D, -14/3D, 1/6D, 61/3D},
+                {2D, 0D, 0D, -1D, 0D, 4D}
+        };
+
+        double[][] inverse = Answer.invert(input);
+
+        for (int index = 0; index < expectedInverse.length; ++index) {
+            Assert.assertArrayEquals(expectedInverse[index], inverse[index], 0.0000001);
+        }
+    }
+
+    @Test
+    public void matrix7x7InversionTest() {
+        double[][] input = new double[][]{
+                {-10D, 0D, 6D, 0D, 0D, -6D, 0D},
+                {-49D, -4D, -26D, 10D, 4D, 28D, -12D},
+                {-6D, 0D, 5D, 0D, 0D, -3D, 0D},
+                {-18D, 3D, 9D, -3D, -2D, -11D, 6D},
+                {17D, -3D, -9D, 5D, 4D, 10D, -6D},
+                {12D, 0D, -6D, 0D, 0D, 8D, 0D},
+                {-30D, 3D, 15D, -6D, -3D, -17D, 8D}
+        };
+
+        double[][] expectedInverse = new double[][]{
+                {-11/2D, 0D, 3D, 0D, 0D, -3D, 0D},
+                {5513/4D, -5/2D, -1505/2D, 2D, -1D, 1503/2D, -6D},
+                {-3D, 0D, 2D, 0D, 0D, -3/2D, 0D},
+                {-821D, 3/2D, 897/2D, -1/2D, 1/2D, -1791/4D, 3D},
+                {3285/4D, -3/2D, -897/2D, 1D, 0D, 448D, -3D},
+                {6D, 0D, -3D, 0D, 0D, 7/2D, 0D},
+                {-6615/8D, 3/2D, 903/2D, -3/4D, 3/4D, -3605/8D, 7/2D}
         };
 
         double[][] inverse = Answer.invert(input);
