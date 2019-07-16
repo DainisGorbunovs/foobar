74c7e734b30ad118e845c0a897c51b5be317d514
Dainis Gorbunovs <dgdev@protonmail.com>
2019-07-17 00:28:52 +0100

    Dodge the Laser solved

--- src/dodge_the_lasers/Solution.java
+++ src/dodge_the_lasers/Solution.java
@@ -1,8 +1,64 @@
 package dodge_the_lasers;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.lang.Math;
+
+import static java.math.BigDecimal.ROUND_HALF_UP;
+
 public class Solution {
+    static BigDecimal sqrt(BigDecimal A, final int SCALE) {
+        // https://stackoverflow.com/questions/13649703/square-root-of-bigdecimal-in-java/19743026#19743026
+        BigDecimal x0 = BigDecimal.ZERO;
+        BigDecimal x1 = new BigDecimal(Math.sqrt(A.doubleValue()));
+        while (!x0.equals(x1)) {
+            x0 = x1;
+            x1 = A.divide(x0, SCALE, ROUND_HALF_UP);
+            x1 = x1.add(x0);
+            x1 = x1.divide(BigDecimal.valueOf(2), SCALE, ROUND_HALF_UP);
+        }
+        return x1;
+    }
+
+    static BigInteger findFraction(BigInteger n) {
+        BigDecimal TWO = BigDecimal.valueOf(2);
+        BigDecimal fractPart = sqrt(TWO, 101).subtract(BigDecimal.ONE);
+        BigDecimal fractPartN = new BigDecimal(n).multiply(fractPart);
+
+        return fractPartN.toBigInteger();
+    }
+
+    // find sum of fractional parts
+    static BigInteger findSum(BigInteger n) {
+        if (n.equals(BigInteger.ZERO))
+            return BigInteger.ZERO;
+        BigInteger TWO = BigInteger.valueOf(2);
+
+        BigInteger sum = BigInteger.ZERO;
+        sum = sum.add(
+                n.multiply(n.add(BigInteger.ONE))
+                        .divide(TWO)
+        );
+
+        BigInteger flooredN = findFraction(n);
+        sum = sum.add(
+                n.multiply(flooredN)
+        );
+
+        sum = sum.subtract(
+                flooredN.multiply(flooredN.add(BigInteger.ONE))
+                        .divide(TWO)
+        );
+
+        sum = sum.subtract(findSum(flooredN));
+
+        return sum;
+    }
+
     public static String solution(String s) {
-        // Your code here
-        return "";
+        BigInteger number = new BigInteger(s);
+        BigInteger sum = findSum(number);
+
+        return sum.toString();
     }
 }
--- test/dodge_the_lasers/SolutionTest.java
+++ test/dodge_the_lasers/SolutionTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class SolutionTest {
     @Test
     public void caseATest() {
+        System.out.println(Solution.solution("19493958214737295721949395821473729572194939582147372957219493958214737295721949395821473729572194939"));
         Assert.assertEquals("4208", Solution.solution("77"));
     }
 
@@ -14,4 +15,26 @@ public class SolutionTest {
         Assert.assertEquals("19", Solution.solution("5"));
     }
 
+    @Test
+    public void caseCTest() {
+        Assert.assertEquals("707314", Solution.solution("1000"));
+    }
+
+    @Test
+    public void caseDTest() {
+        Assert.assertEquals("707106781186547524400844362125559717403" +
+                "49069012855847279",
+                Solution.solution("10000000000000000000000000000"));
+    }
+
+    @Test
+    public void caseETest() {
+        Assert.assertEquals("26871076405196362991682450001989958376" +
+                "308701027080619461439377264770269805701965122904992187884764" +
+                "349301791878515079704272160373589383705408343670901558527604" +
+                "7156809792391683564861028746778522878469385",
+                Solution.solution("1949395821473729572194939582147372957219" +
+                        "49395821473729572194939582147372957219493958214737295" +
+                        "72194939"));
+    }
 }
